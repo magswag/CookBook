98618ed3ec7553ff7e4ec80318b1b85ded24c538
magswag <magnus.o.randby@gmail.com>
2015-10-31 14:43:01 +0100

    -Changed ALOT of things...
    Signed-off-by: magswag <magnus.o.randby@gmail.com>

--- CookBookApp/app/src/main/java/com/micropop/cookbook/PannekakerActivity.java
+++ CookBookApp/app/src/main/java/com/micropop/cookbook/PannekakerActivity.java
@@ -1,36 +1,64 @@
 package com.micropop.cookbook;
 
 import android.app.ActionBar;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.readystatesoftware.systembartint.SystemBarTintManager;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
 
 public class PannekakerActivity extends AppCompatActivity {
     private float mActionBarHeight;
     private ActionBar mActionBar;
-    String EXTRA_IMAGE;
+    private static final String EXTRA_TITLE = "com.micropop.cookbook.extraTitle";
+    private static final String EXTRA_IMAGE = "com.micropop.cookbook.extraImage";
+    private static final String EXTRA_DO = "com.micropop.cookbook.extraDo";
+    private static final String EXTRA_NEED = "com.micropop.cookbook.extraNeed";
+public TextView Needz;
+    public TextView Dooz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_pannekaker);
-
+        setContentView(R.layout.activity_d);
+        String itemTitle = getIntent().getStringExtra(EXTRA_TITLE);
+        String itemNeed = getIntent().getStringExtra(EXTRA_NEED);
+        String itemDo = getIntent().getStringExtra(EXTRA_DO);
+        Dooz = (TextView) findViewById(R.id.yoDoThis);
+        Needz = (TextView) findViewById(R.id.yoNeedThis);
+        Dooz.setText(itemDo);
+        Needz.setText(itemNeed);
+        Bundle extras = getIntent().getExtras();
+        if (extras == null)
+        {
+            return;
+        }
+        int res = extras.getInt("resourseInt");
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        collapsingToolbarLayout.setTitle(getResources().getString(R.string.title_activity_pannekaker));
+        collapsingToolbarLayout.setTitle(itemTitle);
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(R.color.transparentcolor));
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-
-
-
+        final ImageView image = (ImageView) findViewById(R.id.imagennn);
+image.setImageResource(res);
     }
 
     @Override
@@ -47,11 +75,13 @@ public class PannekakerActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
